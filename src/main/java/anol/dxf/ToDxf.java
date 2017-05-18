43c6bb4072406ea848acbee3697229c493ce9a7f
anol <aeolsen@gmail.com>
2017-05-18 22:14:11 +0200

    Holdt på med DXF-genereringstesten.

--- src/main/java/anol/dxf/ToDxf.java
+++ src/main/java/anol/dxf/ToDxf.java
@@ -26,28 +26,9 @@ public class ToDxf {
         writer.write(code + "\n" + format.format(number) + "\n");
     }
 
-    /*
-    WriteDXFPolygon creates a minimal DXF file that only contains
-    the ENTITIES section. This subroutine requires five parameters,
-    the DXF file name, the number of sides for the polygon, the X
-    and Y coordinates for the bottom end of the right-most side
-    (it starts in a vertical direction), and the length for each
-    side. Note that because this only requests 2D points, it does
-    not include the Z coordinates (codes 30 and 31). The lines are
-    placed on the layer "Polygon."
-    */
-    public static void WriteDXFPolygon(BufferedWriter writer, Integer iSides, Double dblX, Double dblY, Double dblLen) throws IOException {
-        Double dblZ = 0.0;
-        Double dblPI = atan(1) * 4;
-        Double dblA1 = (2 * dblPI) / iSides;
-        Double dblA = dblPI / 4;
-        Double bulge = tan(dblA1 / 4);
-        dblA1 = dblA1 / 2;
+    public static void writeDXFProlog(BufferedWriter writer) throws IOException {
+        //
         int layer = 0;
-        int extraVertexCreatedByCurveFitting = 1 + 3 + 8;
-        Double radius = dblLen / 10;
-        Double length = dblLen - 2 * radius;
-        dblX -= radius;
         String name = "P";
         // Header Section
         print(writer, 0, "SECTION");
@@ -58,12 +39,49 @@ public class ToDxf {
         // Entities Section
         print(writer, 0, "SECTION");
         print(writer, 2, "ENTITIES");
+    }
+
+    public static void writeDXFEpilog(BufferedWriter writer) throws IOException {
+        // End of Section
+        print(writer, 0, "ENDSEC");
+        // End of File
+        print(writer, 0, "EOF");
+    }
+
+    // See: https://www.autodesk.com/techpubs/autocad/acad2000/dxf/polyline_dxf_06.htm
+
+    /*
+    WriteDXFPolygon creates a minimal DXF file that only contains
+    the ENTITIES section. This subroutine requires five parameters,
+    the DXF file name, the number of sides for the polygon, the X
+    and Y coordinates for the bottom end of the right-most side
+    (it starts in a vertical direction), and the length for each
+    side. Note that because this only requests 2D points, it does
+    not include the Z coordinates (codes 30 and 31). The lines are
+    placed on the layer "Polygon."
+    */
+    public static void writeDXFPolygon(BufferedWriter writer, Integer iSides, Double dblX, Double dblY, Double dblLen) throws IOException {
+        //
+        int layer = 0;
+        String name = "P";
+        //
+        Double dblZ = 0.0;
+        Double dblPI = atan(1) * 4;
+        Double dblA1 = (2 * dblPI) / iSides;
+        Double dblA = dblPI / 4;
+        Double bulge = tan(dblA1 / 4);
+        dblA1 = dblA1 / 2;
+        Double radius = dblLen / 10;
+        Double length = dblLen - 2 * radius;
+        dblX -= radius;
         // Polyline Entity
         print(writer, 0, "POLYLINE");
         print(writer, 5, name);
         print(writer, 8, layer);
         print(writer, 66, 1);
-        print(writer, 70, 1);
+        print(writer, 39, 10); // Thickness (optional; default = 0)
+        print(writer, 70, 1); // 70 = Polyline flag (bit-coded); default is 0:
+        // 1 = This is a closed polyline (or a polygon mesh closed in the M direction).
         print(writer, 10, 0.0);
         print(writer, 20, 0.0);
         print(writer, 30, 0.0);
@@ -93,9 +111,5 @@ public class ToDxf {
         print(writer, 0, "SEQEND");
         print(writer, 5, "End_" + name);
         print(writer, 8, layer);
-        // End of Section
-        print(writer, 0, "ENDSEC");
-        // End of File
-        print(writer, 0, "EOF");
     }
 }
--- src/test/java/anol/dxf/ToDxfTest.java
+++ src/test/java/anol/dxf/ToDxfTest.java
@@ -1,6 +1,7 @@
 package anol.dxf;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -9,19 +10,33 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static anol.dxf.ToDxf.writeDXFEpilog;
+import static anol.dxf.ToDxf.writeDXFPolygon;
+import static anol.dxf.ToDxf.writeDXFProlog;
+
 class ToDxfTest {
 
     @Test
-    void writeDXFPolygon() {
+    public void writeDXFPolygonTest() {
         // Using nio.file
         Path path = Paths.get("output.dxf");
         Charset charset = Charset.forName("US-ASCII");
         try (BufferedWriter writer = Files.newBufferedWriter(path, charset)) {
+            writeDXFProlog(writer);
+            //
             Integer iSides = 4;
             Double dblX = 0.0;
             Double dblY = 0.0;
             Double dblLen = 100.0;
-            anol.dxf.ToDxf.WriteDXFPolygon(writer, iSides, dblX, dblY, dblLen);
+            writeDXFPolygon(writer, iSides, dblX, dblY, dblLen);
+            //
+            iSides = 5;
+            dblX = -40.0;
+            dblY = 40.0;
+            dblLen = 20.0;
+            writeDXFPolygon(writer, iSides, dblX, dblY, dblLen);
+            //
+            writeDXFEpilog(writer);
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);
         }
